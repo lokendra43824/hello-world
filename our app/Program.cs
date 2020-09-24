67161d4648a74ada77f0e408d9d105bfc7bc9824
Lokendra <lokilokendra12345@gmail.com>
2020-09-24 17:22:57 +0530

    first commit

---
+++ our app/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace our_app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("hello Tirupathi");
+        }
+    }
+}
